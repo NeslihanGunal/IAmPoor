fb773d6c88cd3f43904d533bd7f426545ebf3339
Neslihan Serengil <neslihangunal@gmail.com>
2022-02-27 15:30:10 +0300

    First Commit

---
+++ IAmPoor/IAmPoor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IAmPoor
+//
+//  Created by Neslihan Günal on 27.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
